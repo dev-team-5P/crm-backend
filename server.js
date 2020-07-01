c50d623957891d612e97a45aa7d11892c4afa0a9
achrafHaddad <achrafmail7@gmail.com>
2020-07-01 10:13:14 +0100

    init commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/gestion", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected"))
+  .catch((err) => console.log("error", err));
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.set("port", port);
+
+app.listen(port);
