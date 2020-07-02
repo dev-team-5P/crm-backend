af8355a4a783e34aa543b386f0bb0d5ee8cb24f7
Ahmed-MOOTI <58347415+Ahmed-MOOTI@users.noreply.github.com>
2020-07-02 14:03:48 +0200

    Majuscule

--- models/ActivitySchema.js
+++ models/ActivitySchema.js
@@ -4,4 +4,4 @@ const ActivitySchema = new mongoose.Schema({
 name : {type : String , required: true}} ,
 { timestamps: { createdAt: 'created_at' } });
 
-module.exports = mongoose.model("activity" , ActivitySchema);
+module.exports = mongoose.model("Activity" , ActivitySchema);
