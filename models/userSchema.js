f3c69a8baeb49bf41c199957bbd2fdc063af5e43
achrafHaddad <achrafmail7@gmail.com>
2020-07-17 12:02:10 +0100

    fix fournis for admin

--- models/userSchema.js
+++ models/userSchema.js
@@ -4,7 +4,7 @@ const user = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type:String, default:'user'},
+  role: { type: String, default: "user" },
   resetToken: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "RestToken",
